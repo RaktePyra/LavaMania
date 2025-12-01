9e819704ff1c207875cbe2a44df8dddab6e1b933
RaktePyra <167292813+RaktePyra@users.noreply.github.com>
2025-12-01 14:17:55 +0100

    Initial Project Commit

---
+++ src/client/java/lavamaniareloaded/LavaManiaClient.java
@@ -0,0 +1,10 @@
+package lavamaniareloaded;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LavaManiaClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
