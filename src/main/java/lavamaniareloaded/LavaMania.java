19e6604738766667056d7a13b72477230a81ebc2
Dylan Distexhe <159441555+Distexhe@users.noreply.github.com>
2025-12-01 21:20:45 +0100

    add fonction for initilise block

--- src/main/java/lavamaniareloaded/LavaMania.java
+++ src/main/java/lavamaniareloaded/LavaMania.java
@@ -18,7 +18,7 @@ public class LavaMania implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
+        AddBlockMod.Initialise();
 		LOGGER.info("Hello LavaManiouzze!");
 	}
 }
