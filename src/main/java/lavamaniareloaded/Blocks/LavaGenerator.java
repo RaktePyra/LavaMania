c71514327b0f92594a72930aa748579cc6fe8ddc
Dylan Distexhe <159441555+Distexhe@users.noreply.github.com>
2025-12-02 12:10:17 +0100

    laod et reload donnée in scene and use tick on entityblock

--- src/main/java/lavamaniareloaded/Blocks/LavaGenerator.java
+++ src/main/java/lavamaniareloaded/Blocks/LavaGenerator.java
@@ -1,6 +1,7 @@
 package lavamaniareloaded.Blocks;
 
 import com.mojang.serialization.MapCodec;
+import lavamaniareloaded.ModBlockEntity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
@@ -8,7 +9,10 @@ import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseEntityBlock;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueOutput;
 import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.Nullable;
 
@@ -43,5 +47,12 @@ public class LavaGenerator extends BaseEntityBlock
         player.displayClientMessage(Component.literal("You've clicked the block for the " + BlockEntity.getClicks() + "th time."), true);
 
         return InteractionResult.SUCCESS;
+    }
+
+    @Nullable
+    @Override
+    public <T extends BlockEntity> BlockEntityTicker<T> getTicker(Level world, BlockState state, BlockEntityType<T> type)
+    {
+        return createTickerHelper(type, ModBlockEntity.LAVA_GENERATOR_ENTITY, LavaGeneratorEntity::tick);
     }
 }
--- src/main/java/lavamaniareloaded/Blocks/LavaGeneratorEntity.java
+++ src/main/java/lavamaniareloaded/Blocks/LavaGeneratorEntity.java
@@ -2,9 +2,16 @@ package lavamaniareloaded.Blocks;
 
 import lavamaniareloaded.AddBlockMod;
 import lavamaniareloaded.ModBlockEntity;
+import net.fabricmc.fabric.api.lookup.v1.block.BlockApiLookup;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.storage.ValueInput;
+import net.minecraft.world.level.storage.ValueOutput;
+import org.jetbrains.annotations.Nullable;
 
 public class LavaGeneratorEntity extends BlockEntity
 {
@@ -23,5 +30,33 @@ public class LavaGeneratorEntity extends BlockEntity
     {
         clicks++;
         setChanged();
+    }
+
+    @Override
+    protected void saveAdditional(ValueOutput writeView)
+    {
+        writeView.putInt("clicks", clicks);
+        writeView.putInt("ticksSinceLast", ticksSinceLast);
+
+        super.saveAdditional(writeView);
+    }
+    private int ticksSinceLast = 0;
+    @Override
+    protected void loadAdditional(ValueInput readView) {
+        super.loadAdditional(readView);
+
+        clicks = readView.getIntOr("clicks", 0);
+        ticksSinceLast = readView.getIntOr("ticksSinceLast",0);
+    }
+    
+    public static void tick(Level world, BlockPos blockPos, BlockState blockState, LavaGeneratorEntity entity)
+    {
+        entity.ticksSinceLast++;
+        if(entity.ticksSinceLast > 200)
+        {
+            entity.ticksSinceLast = 0;
+        }
+        System.out.println(entity.ticksSinceLast);
+
     }
 }
