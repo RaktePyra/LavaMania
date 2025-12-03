d65db3e6c86de3b6a35ef973ca841a26d6203ba8
RaktePyra <167292813+RaktePyra@users.noreply.github.com>
2025-12-03 09:54:29 +0100

    Update IEnergyStorage.java

--- src/main/java/lavamaniareloaded/IEnergyStorage.java
+++ src/main/java/lavamaniareloaded/IEnergyStorage.java
@@ -1,5 +1,6 @@
 package lavamaniareloaded;
 
 public interface IEnergyStorage {
-    public void PushEnergy(IEnergyStorage destination);
+    public void PushEnergy(IEnergyStorage destination,int EnergyAmount);
+    public int GetStoredEnergyAmount();
 }
